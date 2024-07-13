a4929472eaffde7fdfcff9f9b5d64eac973f5c86
barash777 <barash.vertihvost@gmail.com>
2024-07-13 16:18:05 +0300

    update tests for lesson 5

--- test/Payments.test.ts
+++ test/Payments.test.ts
@@ -1,17 +1,17 @@
-import { loadFixture, ethers, expect } from "./setup";
+import { loadFixture, ethers, expect } from './setup';
 
-describe("Payments contract", () => {
+describe('Payments contract', () => {
   async function deploy() {
     const [user1, user2] = await ethers.getSigners();
 
-    const Factory = await ethers.getContractFactory("Payments");
+    const Factory = await ethers.getContractFactory('Payments');
     const payments = await Factory.deploy();
     await payments.waitForDeployment();
 
     return { user1, user2, payments };
   }
 
-  it("should be deplyed", async () => {
+  it('should be deplyed', async () => {
     const { payments } = await loadFixture(deploy);
 
     // console.log(user1.address);
@@ -21,35 +21,33 @@ describe("Payments contract", () => {
     expect(payments.target).to.be.properAddress;
   });
 
-  it("should have 0 ETH by default", async () => {
+  it('should have 0 ETH by default', async () => {
     const { payments } = await loadFixture(deploy);
 
     expect(await payments.currentBalance()).to.eq(0);
     expect(await ethers.provider.getBalance(payments.target)).to.be.eq(0);
   });
 
-  it("should be possible to send funds", async () => {
-    const { user1, user2, payments } = await loadFixture(deploy);
+  it('should be possible to send funds', async () => {
+    const { user1, payments } = await loadFixture(deploy);
 
     const sum = 100; // wei
-    const msg = "hellow form hh";
+    const msg = 'hellow form hh';
 
     await payments.pay(msg, { value: sum });
 
     const contractBalance = await ethers.provider.getBalance(payments.target);
     const user1Balance = await ethers.provider.getBalance(user1.address);
-    const user2Balance = await ethers.provider.getBalance(user2.address);
 
-    expect(contractBalance).to.be.eq(100);
-    expect(user1Balance).to.be.lessThan(10000000000000000000000n);
-    expect(user2Balance).to.be.eq(10000000000000000000000n);
+    await expect(contractBalance).to.be.eq(100);
+    await expect(user1Balance).to.be.lessThan(10000000000000000000000n);
   });
 
-  it("should be paid by user2", async () => {
+  it('should be paid by user2', async () => {
     const { user2, payments } = await loadFixture(deploy);
 
     const sum = 100; // wei
-    const msg = "hellow form hh";
+    const msg = 'hellow form hh';
 
     const tx = await payments.connect(user2).pay(msg, { value: sum });
     await tx.wait(1);
@@ -57,11 +55,11 @@ describe("Payments contract", () => {
     await expect(tx).to.changeEtherBalance(user2, -sum);
   });
 
-  it("payments map should be updated after tx", async () => {
+  it('payments map should be updated after tx', async () => {
     const { user2, payments } = await loadFixture(deploy);
 
     const sum = 100; // wei
-    const msg = "hellow form hh";
+    const msg = 'hellow form hh';
 
     const tx = await payments.connect(user2).pay(msg, { value: sum });
     await tx.wait(1);

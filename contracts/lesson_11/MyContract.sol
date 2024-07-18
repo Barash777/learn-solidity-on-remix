d474583c787558a104ca64fad2b668f9b8d3886d
Barash777 <barash.vertihvost@gmail.com>
2024-07-18 13:41:09 +0300

    Lesson 11 part 2.5

---
+++ contracts/lesson_11/MyContract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Ownable.sol";
+import "./Balances.sol";
+
+contract MyContract is Ownable, Balances {
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+    function withdraw(address payable _to) public override(Ownable, Balances) onlyOwner {
+        // Balances.withdraw(_to); // equal to 'super.withdraw(_to);'
+        // Ownable.withdraw(_to);
+        require(_to != address(0), "zero address");
+        super.withdraw(_to); // parent one level above
+    }
+}

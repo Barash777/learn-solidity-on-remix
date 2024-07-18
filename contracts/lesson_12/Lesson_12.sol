6bc2bb26c5cd576d80a7be9857583e695e4c2267
Barash777 <barash.vertihvost@gmail.com>
2024-07-18 13:58:36 +0300

    Lesson 12 part 1

---
+++ contracts/lesson_12/Lesson_12.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./ILogger.sol";
+
+contract Lesson_12 {
+    ILogger logger;
+   
+    constructor(address _logger) {
+        logger = ILogger(_logger);
+    }
+
+    function getPayment(address _from, uint _index) public view returns(uint) {
+        return logger.getEntry(_from, _index);
+    }
+
+    receive() external payable {
+        logger.log(msg.sender, msg.value);
+    }
+}

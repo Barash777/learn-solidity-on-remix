15d886dd024e3818180484753bc7c66c52f8f12c
Barash777 <barash.vertihvost@gmail.com>
2024-07-18 17:58:02 +0300

    Lesson 12 part 2

---
+++ contracts/lesson_12/LibDemo.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./LibraryExt.sol";
+
+contract LibDemo {
+    using LibraryStrExt for string;
+    using LibraryArrayExt for uint[];
+
+    function compareStrings(string memory str1, string memory str2) public pure returns(bool) {
+        return str1.eq(str2);
+    }
+
+    function findElement(uint[] memory array, uint element) public pure returns(bool) {
+        return array.inArray(element);
+    }
+}

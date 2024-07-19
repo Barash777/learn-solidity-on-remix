290ed1994d1e6021268f10d9e9db2ae22174616b
Barash777 <barash.vertihvost@gmail.com>
2024-07-19 17:27:22 +0300

    SP task 2

---
+++ summer_practicum/task_2/Task_2.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Task_2 {
+    address owner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address payable owner_2;
+    // string str = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    string str = "Hello world";
+}

ea48cb33571ad0b87061b14e706282462e6676e7
Barash777 <barash.vertihvost@gmail.com>
2024-07-24 14:47:49 +0300

    SP task 7

---
+++ summer_practicum/task_7/case_1/BarashToken.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./BarashERC.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BarashToken is BarashERC, Ownable {
+    constructor() BarashERC("Barash", "BRSH", 6) Ownable(msg.sender) {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    /**
+     * @dev Creates a `value` amount of tokens and assigns them to `account`, by transferring it from address(0).
+     * Relies on the `_update` mechanism
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * NOTE: This function is not virtual, {_update} should be overridden instead.
+     */
+    function _mint(address account, uint256 value) internal {
+        require(
+            account != address(0),
+            "Account to mint can't be zero address!"
+        );
+        _update(address(0), account, value);
+    }
+
+    /**
+     * @dev Destroys a `value` amount of tokens from `account`, lowering the total supply.
+     * Relies on the `_update` mechanism.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * NOTE: This function is not virtual, {_update} should be overridden instead
+     */
+    function _burn(address account, uint256 value) internal {
+        require(
+            account != address(0),
+            "Account to burn can't be zero address!"
+        );
+        _update(account, address(0), value);
+    }
+}
---
+++ summer_practicum/task_7/case_2/BarashToken2.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact barash.vertihvost@gmail.com
+contract Barash is ERC20, Ownable {
+    constructor(address initialOwner)
+        ERC20("Barash", "BRSH")
+        Ownable(initialOwner)
+    {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+        _burn(msg.sender, totalSupply()/2);
+    }
+}

d5e2a716645e94acbe2c1040372f82f33949177e
sergei-deriv <sergei@deriv.com>
2024-08-15 17:00:46 +0300

    have some questions

--- summer_practicum/task_9/Market.sol
+++ summer_practicum/task_9/Market.sol
@@ -1,34 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./AnythingToken.sol";
 import "./SomethingToken.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Market is Ownable {
-    // SomethingToken private _someToken = new SomethingToken();
-    // AnythingToken private _anyToken = new AnythingToken();
-    SomethingToken public _someToken;
-    AnythingToken public _anyToken;
-
-    // IERC20 public token;
+    IERC20 public _someToken;
 
     constructor(
-        address somethingTokenAddress,
-        address anythingTokenAddress
+        address somethingTokenAddress
+        // address anythingTokenAddress
     ) Ownable(msg.sender) {
-        _someToken = SomethingToken(somethingTokenAddress);
-        _anyToken = AnythingToken(anythingTokenAddress);
+        _someToken = IERC20(somethingTokenAddress);
+        // _someToken = IERC20(0x9a2E12340354d2532b4247da3704D2A5d73Bd189);
+        // _someToken = SomethingToken(somethingTokenAddress);
+
+        // type(uint256).max
+        // _someToken.approve(address(this), type(uint256).max);
     }
 
-    receive() external payable {
-        uint tokensToBuy = msg.value;
-        require(tokensToBuy > 0, "not enough funds!");
+    function getSomeBalance(address ad) public view returns (uint256) {
+        return _someToken.balanceOf(ad);
+    }
+    
+    function ApproveWEIRDSomeTokens() public {
+        _someToken.approve(address(this), 100);
+    }
 
-        require(
-            token.balanceOf(address(this)) >= tokensToBuy,
-            "not enough tokens!"
+    function getWEIRDSomeAllowance() view public returns (uint256) {
+        return _someToken.allowance(address(this), address(this));
+    }
+
+    function ApproveSomeTokens() public {
+        // _someToken.approve(address(this), 100);
+        (bool success, ) = address(_someToken).delegatecall(
+            abi.encodeWithSignature("approve(address,uint256)", address(this), 100)
+            // abi.encodeWithSelector(IERC20.approve.selector, address(this), 100)
+            // abi.encodeWithSelector(_someToken.approve.selector, address(this), 100)
         );
 
-        // token.transfer(msg.sender, tokensToBuy);
+        require(success, "failed!");
+    }
+
+    function getSomeAllowance() public view returns (uint256) {
+        return _someToken.allowance(msg.sender, address(this));
     }
 }

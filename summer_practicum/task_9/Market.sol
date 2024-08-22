d96adc953cd0690c6daa6a9410865afa75f60c5f
sergei-deriv <sergei@deriv.com>
2024-08-22 11:27:28 +0300

    intermediate result

--- summer_practicum/task_9/Market.sol
+++ summer_practicum/task_9/Market.sol
@@ -1,48 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./SomethingToken.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Market is Ownable {
     IERC20 public _someToken;
+    IERC20 public _anyToken;
+    
+    address mainOwner = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    uint8 tokenPrice = 1;
+
+    event TokensPurchased(address indexed buyer, uint256 amount);
+    event TokensWithdrawn(address indexed owner, uint256 amount);
+    event TokenPriceUpdated(uint256 oldPrice, uint256 newPrice);
 
     constructor(
-        address somethingTokenAddress
-        // address anythingTokenAddress
+        // IERC20 somethingTokenAddress,
+        // IERC20 anythingTokenAddress
     ) Ownable(msg.sender) {
-        _someToken = IERC20(somethingTokenAddress);
-        // _someToken = IERC20(0x9a2E12340354d2532b4247da3704D2A5d73Bd189);
-        // _someToken = SomethingToken(somethingTokenAddress);
-
-        // type(uint256).max
-        // _someToken.approve(address(this), type(uint256).max);
+        // _someToken = IERC20(somethingTokenAddress);
+        // _anyToken = IERC20(anythingTokenAddress);
+        _someToken = IERC20(0xb4a88c4086055b8f546911BaaED28b7CEE134EF9);
+        _anyToken = IERC20(0xE645D1e7bDA5Ef4932cDAA11394fA984dd4F2fb9);
     }
 
-    function getSomeBalance(address ad) public view returns (uint256) {
-        return _someToken.balanceOf(ad);
-    }
-    
-    function ApproveWEIRDSomeTokens() public {
-        _someToken.approve(address(this), 100);
+    function buySomeTokens(uint256 _value) payable public {
+        // bool success = _someToken.transferFrom(mainOwner, msg.sender, _value);
+        // require(success, "Something went wrong");
+        buyTokens(_someToken, _value);
     }
 
-    function getWEIRDSomeAllowance() view public returns (uint256) {
-        return _someToken.allowance(address(this), address(this));
+    function buyAnyTokens(uint256 _value) payable public {
+        buyTokens(_anyToken, _value);
     }
 
-    function ApproveSomeTokens() public {
-        // _someToken.approve(address(this), 100);
-        (bool success, ) = address(_someToken).delegatecall(
-            abi.encodeWithSignature("approve(address,uint256)", address(this), 100)
-            // abi.encodeWithSelector(IERC20.approve.selector, address(this), 100)
-            // abi.encodeWithSelector(_someToken.approve.selector, address(this), 100)
-        );
+    // function approveAllTokensForMarket() public onlyOwner {
+    //     // _someToken.approve(address(this), type(uint256).max);
+    //     (bool success, ) = address(_someToken).delegatecall(
+    //         abi.encodeWithSignature("approve(address,uint256)", address(this), 100)
+    //         // abi.encodeWithSelector(IERC20.approve.selector, address(this), 100)
+    //         // abi.encodeWithSelector(_someToken.approve.selector, address(this), 100)
+    //     );
 
-        require(success, "failed!");
-    }
+    //     require(success, "failed!");
+    // }
 
-    function getSomeAllowance() public view returns (uint256) {
-        return _someToken.allowance(msg.sender, address(this));
+    function buyTokens(IERC20 token, uint256 tokenAmount) public payable {
+        uint256 cost = tokenAmount * tokenPrice;
+        require(msg.value >= cost, "Insufficient Ether sent");
+
+        uint256 allowance = token.allowance(owner(), address(this));
+        require(allowance >= tokenAmount, "Not enough tokens approved for transfer");
+
+        token.transferFrom(owner(), msg.sender, tokenAmount);
+        emit TokensPurchased(msg.sender, tokenAmount);
+
+        // if (msg.value > cost) {
+        //     payable(msg.sender).transfer(msg.value - cost); // Refund overpayment
+        // }
     }
 }
--- summer_practicum/task_9/Task9_ERC20_1.sol
+++ summer_practicum/task_9/Task9_ERC20_1.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-abstract contract Task9_ERC20_1 is ERC20 {
+abstract contract Task9_ERC20_1 is ERC20, Ownable {
     uint8 private _decimals;
 
     constructor(
@@ -12,12 +12,16 @@ abstract contract Task9_ERC20_1 is ERC20 {
         string memory symbol_,
         uint8 decimals_,
         uint256 initTokensCount
-    ) ERC20(name_, symbol_) {
+    ) Ownable(msg.sender) ERC20(name_, symbol_)  {
         _decimals = decimals_;
         _mint(msg.sender, initTokensCount * 10 ** decimals());
     }
 
     function decimals() public view override(ERC20) returns (uint8) {
         return _decimals;
+    }
+
+    function approveAllTokensForMarket(address _marketAddress) public onlyOwner {
+        approve(_marketAddress, type(uint256).max);
     }
 }
